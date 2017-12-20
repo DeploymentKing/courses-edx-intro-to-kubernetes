50cf94c7c118e2b1e2b8904ef20932e82520aa0d
Lee Myring <mail@thinkstack.io>
2017-12-20 12:20:16 +0000

    Add chapter 10 commands and yaml config

---
+++ chapter_10.0.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Using the hostPath Volume Type Demo
+minikube start
+minikube status
+minikube dashboard
+
+minikube ssh
+# The following commands are executed on the minikube host
+mkdir -p vol
+cd vol
+echo "Welcome to Kubernetes" > index.html
+cat index.html
+pwd # Should return /home/docker/vol
+
+# ctrl + D to exit back to your workstation
+
+kubectl create -f chapter_10.yaml
+kubectl create -f chapter_10_svc.yaml
+
+kubectl get service
+
+kubectl delete service web-service
+kubectl delete deployments webserver
